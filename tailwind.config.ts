5fadaf3d0a992424bd0188d61065d7cc37edd688
Hayden Zeller <hayden@haydenzeller.com>
2024-11-05 20:29:32 -0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        notable: ["Notable", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
